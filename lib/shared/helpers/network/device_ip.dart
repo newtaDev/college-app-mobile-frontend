7840dec193bd005c82321dbd783c8c590d519060
newtaDev <newta.dev@gmail.com>
2022-09-09 14:22:35 +0530

    fix: device_ip findinder bug in andoid

--- lib/shared/helpers/network/device_ip.dart
+++ lib/shared/helpers/network/device_ip.dart
@@ -6,21 +6,25 @@ class DeviceIp {
   static String ipAddress = '192.168.217.105';
 
   static Future<String> getDeviceIpAddress() async {
-    for (final interface in await NetworkInterface.list()) {
-      ipAddress = interface.addresses
-          .where(
-            (element) =>
-                element.address.startsWith('10.0.') ||
-                element.address.startsWith('192.'),
-          )
-          .first
-          .address;
+    try {
+      for (final interface in await NetworkInterface.list()) {
+        ipAddress = interface.addresses
+            .where(
+              (element) =>
+                  element.address.startsWith('10.0.') ||
+                  element.address.startsWith('192.'),
+            )
+            .first
+            .address;
 
-      /// Them condition exectutes for android emulators
-      if (ipAddress.startsWith('10.0.')) {
-        ipAddress = '10.0.2.2';
+        /// Them condition exectutes for android emulators
+        if (ipAddress.startsWith('10.0.')) {
+          ipAddress = '10.0.2.2';
+        }
+        debugPrint(ipAddress);
       }
-      debugPrint(ipAddress);
+    } catch (e) {
+      ipAddress = ipAddress;
     }
     return ipAddress;
   }
