794bda7147cefc71150d98792935a251da93ceab
newtaDev <newta.dev@gmail.com>
2022-09-19 23:40:56 +0530

    reactor: migrated qr_code_scanner to mobile_scanner package

--- lib/presentation/features/qr/scanner/qr_scanner_page.dart
+++ lib/presentation/features/qr/scanner/qr_scanner_page.dart
@@ -1,12 +1,12 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../../../../utils/utils.dart';
 import '../../../router/routes.dart';
+import '../widget/qr_overlay_shap.dart';
 
 class QrScannerPage extends StatefulWidget {
   const QrScannerPage({super.key});
@@ -16,27 +16,24 @@ class QrScannerPage extends StatefulWidget {
 }
 
 class _QrScannerPageState extends State<QrScannerPage> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
   Barcode? result;
-  QRViewController? controller;
+  late final MobileScannerController cameraController;
   final qrNavDebouncer = Debouncer(delay: const Duration(milliseconds: 500));
 
-  // In order to get hot reload to work we need to pause the camera if the platform
-  // is android, or resume the camera if the platform is iOS.
   @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    } else if (Platform.isIOS) {
-      controller!.resumeCamera();
-    }
+  void initState() {
+    cameraController = MobileScannerController(
+      formats: [BarcodeFormat.qrCode],
+      torchEnabled: false,
+    );
+
+    super.initState();
   }
 
   @override
   void dispose() {
     qrNavDebouncer.dispose();
-    controller?.dispose();
+    cameraController.dispose();
     super.dispose();
   }
 
@@ -54,53 +51,78 @@ class _QrScannerPageState extends State<QrScannerPage> {
           'Scan Qr code',
           style: TextStyle(color: Colors.white),
         ),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                if (state == null) {
+                  return const Icon(Icons.flash_off, color: Colors.grey);
+                }
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.blue);
+                }
+              },
+            ),
+            onPressed: cameraController.toggleTorch,
+          ),
+        ],
       ),
-      body: QRView(
-        key: qrKey,
-        onQRViewCreated: _onQRViewCreated,
-        overlay: QrScannerOverlayShape(
-          borderColor: Colors.white,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 5,
-          cutOutSize: scanArea,
-        ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          MobileScanner(
+            controller: cameraController,
+            onDetect: (barcode, args) {
+              if (barcode.rawValue == null) {
+                debugPrint('Failed to scan Barcode');
+              } else {
+                final data = json.decode(barcode.rawValue!);
+                final id = data['id'];
+                final userType = data['userType'];
+                final navigateTo = data['navigateTo'];
+                if (id == null ||
+                    userType == null ||
+                    navigateTo != Routes.profileScreen.name) {
+                  // ScaffoldMessenger.of(context)
+                  //   ..hideCurrentSnackBar()
+                  //   ..showSnackBar(const SnackBar(content: Text('Invalid Qr code')));
+                  return;
+                } else {
+                  if (qrNavDebouncer.isActive ?? false) return;
+                  qrNavDebouncer.run(() {
+                    context
+                      ..pop()
+                      ..goNamed(
+                        Routes.profileScreen.name,
+                        params: {
+                          'profile_id': id,
+                          ...RouteParams.withDashboard
+                        },
+                        queryParams: {'userType': userType},
+                      );
+                  });
+                }
+              }
+            },
+          ),
+          DecoratedBox(
+            decoration: ShapeDecoration(
+              shape: QrScannerOverlayShape(
+                borderColor: Colors.white,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 5,
+                cutOutSize: scanArea,
+              ),
+            ),
+          ),
+        ],
       ),
     );
-  }
-
-  Future<void> _onQRViewCreated(QRViewController controller) async {
-    this.controller = controller;
-
-    controller.scannedDataStream.listen((scanData) {
-      if (scanData.format == BarcodeFormat.qrcode) {
-        final data = json.decode(scanData.code!);
-        final id = data['id'];
-        final userType = data['userType'];
-        final navigateTo = data['navigateTo'];
-        if (id == null ||
-            userType == null ||
-            navigateTo != Routes.profileScreen.name) {
-          // ScaffoldMessenger.of(context)
-          //   ..hideCurrentSnackBar()
-          //   ..showSnackBar(const SnackBar(content: Text('Invalid Qr code')));
-          return;
-        } else {
-          if (qrNavDebouncer.isActive ?? false) return;
-          qrNavDebouncer.run(() {
-            context
-              ..pop()
-              ..goNamed(
-                Routes.profileScreen.name,
-                params: {'profile_id': id, ...RouteParams.withDashboard},
-                queryParams: {'userType': userType},
-              );
-          });
-        }
-      }
-    });
-    if (Platform.isAndroid) {
-      await controller.resumeCamera();
-    }
   }
 }
