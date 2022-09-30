63a2eda502d7e2ce19af7889cd30c0c24dd9b824
newtaDev <newta.dev@gmail.com>
2022-09-30 23:30:05 +0530

    chore: added outfit font

--- packages/styles_lib/lib/assets/src/font_assets.dart
+++ packages/styles_lib/lib/assets/src/font_assets.dart
@@ -4,4 +4,5 @@ part of app_assets;
 class FontAssets {
   static const String poppins = 'packages/styles_lib/Poppins';
   static const String raleway = 'packages/styles_lib/Raleway';
+  static const String outfit = 'packages/styles_lib/Outfit';
 }
